0d31c1c8f6a76cac412341b5af4c2dfe44f26b58
André Carvalho <afacarvalho@yahoo.com.br>
2021-01-21 10:41:53 -0300

    Include the file with the update date in the download script. New data files by December 2020.

--- webapp/data/curl_get_json.sh
+++ webapp/data/curl_get_json.sh
@@ -3,6 +3,7 @@
 URL="http://terrabrasilis.dpi.inpe.br/geoserver/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0"
 URL_PRODES=$URL"&OUTPUTFORMAT=application%2Fjson&TYPENAME=experimentals:fof_amz_d"
 URL_CAR=$URL"&OUTPUTFORMAT=application%2Fjson&TYPENAME=experimentals:fof_car_d"
+URL_DATE=$URL"&OUTPUTFORMAT=application%2Fjson&TYPENAME=experimentals:updated_date"
 
 curl "$URL_PRODES" -H 'User-Agent: cURL from Focuses of Fires dashboard script' \
 -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
@@ -16,3 +17,8 @@ curl "$URL_CAR" -H 'User-Agent: cURL from Focuses of Fires dashboard script' \
 --compressed -H 'Connection: keep-alive' \
 -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' > focuses-of-fires-car.json
 
+curl "$URL_DATE" -H 'User-Agent: cURL from Focuses of Fires dashboard script' \
+-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
+-H 'Accept-Language: en,en-US;q=0.5' \
+--compressed -H 'Connection: keep-alive' \
+-H 'Pragma: no-cache' -H 'Cache-Control: no-cache' > updated-date.json
