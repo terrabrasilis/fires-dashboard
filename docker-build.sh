c3206c8ccac33f44a1ff4cb5cbefcb82830ac410
André Carvalho <afacarvalho@yahoo.com.br>
2021-08-20 14:12:40 -0300

    Improve docker build to use repository tag as image tag

--- docker-build.sh
+++ docker-build.sh
@@ -3,14 +3,14 @@
 # Stopping all containers
 #docker container stop terrabrasilis_focuses_of_fires
 
-VERSION=$(cat webapp/PROJECT_VERSION | grep -oP '(?<="version": ")[^"]*')
+VERSION=$(git describe --tags --abbrev=0)
 export VERSION
 # build all images
-docker build --no-cache -t terrabrasilis/focuses-of-fires-dashboard:v$VERSION --build-arg VERSION=$VERSION --build-arg INDEX_FILE=focuses-of-fires -f environment/Dockerfile .
+docker build --no-cache -t terrabrasilis/focuses-of-fires-dashboard:$VERSION --build-arg VERSION=$VERSION --build-arg INDEX_FILE=focuses-of-fires -f environment/Dockerfile .
 
 # send to dockerhub
 docker login
-docker push terrabrasilis/focuses-of-fires-dashboard:v$VERSION
+docker push terrabrasilis/focuses-of-fires-dashboard:$VERSION
 
 # If you want run containers, uncomment this lines
-#docker run -d --rm -p 84:80 --name terrabrasilis_focuses_of_fires terrabrasilis/focuses-of-fires-dashboard:v$VERSION
+#docker run -d --rm -p 84:80 --name terrabrasilis_focuses_of_fires terrabrasilis/focuses-of-fires-dashboard:$VERSION
