6bd51d9157931d492d73d6ae280682ded0b1e921
André Carvalho <afacarvalho@yahoo.com.br>
2021-09-08 18:45:38 -0300

    Fix image entry point

--- environment/docker-entrypoint.sh
+++ environment/docker-entrypoint.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-if [[ -f "/data/disclaimer-pt-br.txt" ]];
-then
-    # File exists!
-    rm -rf /usr/share/nginx/html/data \
-    && ln -s /data /usr/share/nginx/html/data
-else
-    # File not found.
-    mv /usr/share/nginx/html/data/* /data/ \
-    && rm -rf /usr/share/nginx/html/data \
-    && ln -s /data /usr/share/nginx/html/data
-fi;
-
 # run nginx in foreground
 nginx -g 'daemon off;'
