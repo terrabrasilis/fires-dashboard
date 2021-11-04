7ec670789be3742976cb23b90308c8b46badcc67
André Carvalho <afacarvalho@yahoo.com.br>
2021-11-04 15:10:05 -0300

    Clean and format code

--- webapp/js/terrabrasilis-start.js
+++ webapp/js/terrabrasilis-start.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
-    //$('#myModal').modal('show');
-    
     //carousel
     $('.carousel').carousel({
         interval: 2000
     });
-    
+
     $("#sidebar").mCustomScrollbar({
         theme: "minimal"
     });
@@ -15,9 +13,9 @@ $(document).ready(function () {
         $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
         // no sense because when side bar has class active the return is false
-        if($('#sidebar').hasClass('active')){// if open
+        if ($('#sidebar').hasClass('active')) {// if open
             $('#panel_container').addClass('full-width');
-        }else{//if close
+        } else {//if close
             $('#panel_container').removeClass('full-width');
         }
         graph.doResize();
@@ -27,39 +25,34 @@ $(document).ready(function () {
 /* Altera icone do accordion da tabela */
 
 $(document).ready(function () {
-            $('.collapse')
-                .on('shown.bs.collapse', function() {
-                    $(this)
-                        .parent()
-                        .find(".fa-plus-circle")
-                        .removeClass("fa-plus-circle")
-                        .addClass("fa-minus-circle");
-                })
-                .on('hidden.bs.collapse', function() {
-                    $(this)
-                        .parent()
-                        .find(".fa-minus-circle")
-                        .removeClass("fa-minus-circle")
-                        .addClass("fa-plus-circle");
-                });
+    $('.collapse')
+        .on('shown.bs.collapse', function () {
+            $(this)
+                .parent()
+                .find(".fa-plus-circle")
+                .removeClass("fa-plus-circle")
+                .addClass("fa-minus-circle");
+        })
+        .on('hidden.bs.collapse', function () {
+            $(this)
+                .parent()
+                .find(".fa-minus-circle")
+                .removeClass("fa-minus-circle")
+                .addClass("fa-plus-circle");
         });
+});
 
 $(document).ready(function () {
 
     let versionDiv = $('#version');
-    let hn=document.location.hostname;
-    if(versionDiv.length>0)
-    {
-        if(hn.includes('127.')||hn.includes('localhost'))
+    let hn = document.location.hostname;
+    if (versionDiv.length > 0) {
+        if (hn.includes('127.') || hn.includes('localhost'))
             versionDiv.append('ver: IN_DEV_ENV');
         else
-            $.getJSON('PROJECT_VERSION', function(data) {
+            $.getJSON('PROJECT_VERSION', function (data) {
                 let version = data.version;
-                versionDiv.append('ver: '+version);
+                versionDiv.append('ver: ' + version);
             });
     }
 });
-
-
-
-        
